b20730059e0e852c061d90eb1d09378374d43c2b
MatthiasCrow <42616612+MatthiasCrow@users.noreply.github.com>
2019-12-22 23:23:55 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,4 +26,4 @@ client.on('message', message => {
 
 // THIS  MUST  BE  THIS  WAY
 
-client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
+client.login(process.env.NjU4NTE5MTAxNTU1MDgxMjE4.XgA8Gg.5WHKM0fGFCTuMEhnpaowpCOIk1I);//BOT_TOKEN is the Client Secret
